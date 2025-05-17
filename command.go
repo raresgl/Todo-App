c1c131468aaf3880376e4b54f130946454923d46
rmateiu <raresmateiu@yahoo.com>
2025-05-17 14:45:44 +0300

    Finished the due date functionality

--- command.go
+++ command.go
@@ -4,24 +4,25 @@ import (
 	"flag"
 	"fmt"
 	"os"
-	"strings"
 	"time"
 )
 
 type CmdFlags struct {
-	Add    string
-	Delete int
-	Edit   int
-	Toggle int
-	List   bool
-	Clean  bool
+	Add     string
+	DueDate string
+	Delete  int
+	Edit    int
+	Toggle  int
+	List    bool
+	Clean   bool
 }
 
 func NewCmdFlags() *CmdFlags {
 	cf := CmdFlags{}
 
-	flag.StringVar(&cf.Add, "add", "", "Add a new todo specify title")
-	flag.IntVar(&cf.Edit, "edit", -1, "Edit a to do by index")
+	flag.StringVar(&cf.Add, "add", "", "Add a new todo specify title and optionally --due for due date")
+	flag.StringVar(&cf.DueDate, "due", "", "Specify due date for new todo (DD.MM.YYYY)")
+	flag.IntVar(&cf.Edit, "edit", -1, "Edit a to do by index. Can edit the name or the due date")
 	flag.IntVar(&cf.Delete, "delete", -1, "Specify a todo by index to delete")
 	flag.IntVar(&cf.Toggle, "toggle", -1, "Specify a todo by index to toggle")
 	flag.BoolVar(&cf.List, "list", false, "List all todos")
@@ -36,25 +37,16 @@ func (cf *CmdFlags) Execute(todos *Todos) {
 	case cf.List:
 		todos.print()
 	case cf.Add != "":
-		parts := strings.SplitN(cf.Add, ":", 2)
-		if len(parts) == 2 {
-			dateStr := strings.TrimSpace(parts[1])
-			var dueDate *time.Time
-			if dateStr != "" {
-				parsedTime, err := time.Parse("02.01.2006", dateStr)
-				if err != nil {
-					fmt.Println("Error, invalid format for add. Please use name:due date (DD.LL.YYYY)")
-					os.Exit(1)
-				}
-				dueDate = &parsedTime
+		var dueDate *time.Time
+		if cf.DueDate != "" {
+			parsedTime, err := time.Parse("02.01.2006", cf.DueDate)
+			if err != nil {
+				fmt.Println("Error, invalid format for --due. Please use name:due date (DD.LL.YYYY)")
+				os.Exit(1)
 			}
-			todos.add(parts[0], dueDate)
-		} else if len(parts) == 1 {
-			todos.add(cf.Add, nil)
-		} else {
-			fmt.Println("Error, invalid format for add. Please use name:due date (DD.LL.YYYY)")
-			os.Exit(1)
+			dueDate = &parsedTime
 		}
+		todos.add(cf.Add, dueDate)
 	case cf.Edit != -1:
 		todos.edit(cf.Edit)
 	case cf.Toggle != -1:
